4d84787c38e5ad67c92aaf62756d69f2fdb64118
BullsEye <18664297+BullsEye34@users.noreply.github.com>
2020-07-18 17:06:12 +0530

    Render Card

--- Web/next-up/src/App.js
+++ Web/next-up/src/App.js
@@ -49,29 +49,29 @@ function App() {
           Learn React
         </a> */}
         <div className="Background-image">
-
-        </div>
-        <Card className={classes.root}>
-          <CardContent>
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
-              Word of the Day
+          <Card className={classes.root}>
+            <CardContent>
+              <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Word of the Day
         </Typography>
-            <Typography variant="h5" component="h2">
-              be{bull}nev{bull}o{bull}lent
+              <Typography variant="h5" component="h2">
+                be{bull}nev{bull}o{bull}lent
         </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              adjective
+              <Typography className={classes.pos} color="textSecondary">
+                adjective
         </Typography>
-            <Typography variant="body2" component="p">
-              well meaning and kindly.
+              <Typography variant="body2" component="p">
+                well meaning and kindly.
           <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </div>
+
       </header>
     </div>
   );
