a93109d0de7deefb6d500b244c760d5f8c6a5e5c
BullsEye <18664297+BullsEye34@users.noreply.github.com>
2020-07-18 17:03:45 +0530

    Material UI

--- Web/next-up/src/App.js
+++ Web/next-up/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './App.css';
+import Card from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+        crossorigin="anonymous"
+      />
       <header className="background">
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,7 +28,7 @@ function App() {
 
         </div>
         <Card>
-
+          <Card.Body>This is some text within a card body.</Card.Body>
         </Card>
       </header>
     </div>
