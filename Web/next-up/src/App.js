e78e8c355951a689bd5c847b6cf97535222922d8
BullsEye <18664297+BullsEye34@users.noreply.github.com>
2020-07-18 17:06:26 +0530

    Remove Comment

--- Web/next-up/src/App.js
+++ Web/next-up/src/App.js
@@ -36,18 +36,7 @@ function App() {
         crossorigin="anonymous"
       />
       <header className="background">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
         <div className="Background-image">
           <Card className={classes.root}>
             <CardContent>
