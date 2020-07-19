133b00f8bed1dd2e0e75f8d88c585d4871c19c6e
BullsEye <18664297+BullsEye34@users.noreply.github.com>
2020-07-20 01:14:36 +0530

    Import textfield

--- Web/next-up/src/App.js
+++ Web/next-up/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import { TextField } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import back from './assets/mainBackground.png';
