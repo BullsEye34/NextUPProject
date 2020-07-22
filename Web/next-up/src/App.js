62aaf4815c8e5e458f37ec0534828d85300231e6
BullsEye <18664297+BullsEye34@users.noreply.github.com>
2020-07-23 01:35:08 +0530

    Import Result

--- Web/next-up/src/App.js
+++ Web/next-up/src/App.js
@@ -8,6 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import back from './assets/mainBackground.png';
 import Grid from '@material-ui/core/Grid';
 import Button from "@material-ui/core/Button";
+import Result from './result'
 
 
 
