c34416db96bb4b2e0be6d8b1d149dcadb43efe12
BullsEye <18664297+BullsEye34@users.noreply.github.com>
2020-07-25 00:05:14 +0530

    Make Python Script from ipynb

---
+++ Web/next-up/src/scripts/main.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.preprocessing import LabelEncoder
+
+dataset = pd.read_csv('cleandataset.csv')
+print(dataset)
+le = LabelEncoder()
+
+dataset.Location = le.fit_transform(dataset.Location)
+print(dataset)
+
+ct = ColumnTransformer(
+    [("Location", OneHotEncoder(), [0])], remainder='passthrough')
+dataset = ct.fit_transform(dataset)
+print(dataset)
+
+X = dataset[:, 1:-1]
+y = dataset[:, -1]
+
+print(X)
+print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+
+model = RandomForestRegressor(max_depth=5, random_state=14)
+model.fit(X_train, y_train)
+
+
+print("RANDOM FOREST REGRESSOR")
+
+print("TRAINING DATA ACCURACY - ", end="")
+print(model.score(X_train, y_train))
+
+print("TESTING DATA ACCURACY - ", end="")
+print(model.score(X_test, y_test))
