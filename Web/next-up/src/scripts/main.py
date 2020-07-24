776f66df779e575076c63db6a5538e70bbde58c2
BullsEye <18664297+BullsEye34@users.noreply.github.com>
2020-07-25 00:14:59 +0530

    Write Dummy Func

--- Web/next-up/src/scripts/main.py
+++ Web/next-up/src/scripts/main.py
@@ -15,6 +15,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+
 dataset = pd.read_csv('cleandataset.csv')
 print(dataset)
 le = LabelEncoder()
